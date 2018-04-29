40c3d2adab458e0c428bbc592baaa161dcffcf60
PacoAvelar <ie704217@iteso.mx>
2018-04-29 17:24:43 -0500

    Changes on the range of the values

--- BASADOS_practica2.py
+++ BASADOS_practica2.py
@@ -2,7 +2,7 @@ import numpy as np
 import socket
 from scipy.io import wavfile
 import math
-
+import time
 from scipy.interpolate import interp1d
 
 MESSAGE = np.array([1, 2, 3, 4, 8])
@@ -11,7 +11,7 @@ MESSAGE = np.array([1, 2, 3, 4, 8])
 # MESSAGE = MESSAGE.astype(np.int16)
 
 # socket opening and data sending
-UDP_IP = "192.168.0.102"
+UDP_IP = "192.168.0.105"
 UDP_PORT = 50007
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
@@ -21,13 +21,17 @@ sock = socket.socket(socket.AF_INET,  # internet
 # sock.sendto(MESSAGE, (UDP_IP,UDP_PORT)) #Message sending to the established IP and Port
 
 # read the wav file
-m = interp1d([0, 65535], [0, pow(2,12) -1])
-fs, data = wavfile.read('C:/Users/gabrielpc/Documents/python/rolon_gabo.wav')
+downsample = 1
 
+m = interp1d([0, 65535], [0, pow(2,12) - 100])
+fs, data = wavfile.read('/media/francisco/OS/mambo_no_5-lou_bega.wav')
+fs = fs/downsample
+data = data[0::downsample]
+
+max_samples_per_transfer = 750
 number_of_samples = len(data)
 number_of_bytes = number_of_samples * 2  # total bytes; each sample has 16bits
-max_bytes_per_transfer = 1500  # max amount of bytes to be transferred
-max_samples_per_transfer = int(max_bytes_per_transfer / 2)  # this is because each sample is of 16bits
+max_bytes_per_transfer: int = max_samples_per_transfer*2  # max amount of bytes to be transferred
 
 complete_transfers = math.floor(number_of_bytes / max_bytes_per_transfer)  # the amount of blocks to be transferred
 number_of_bytes_final_transfer = number_of_bytes - complete_transfers * max_bytes_per_transfer
@@ -38,36 +42,36 @@ total_transfers = complete_transfers + 1
 Mb = np.array(np.zeros((total_transfers, max_samples_per_transfer)))
 # for the blocks of 1500 bytes
 
-offset: int = (pow(2, 16) - 1)/2
+offset: int = (pow(2, 16) - 1)/2 +0.5
 n_block: int
-for n_block in range(0, complete_transfers - 1):
-    for sample in range(0, max_samples_per_transfer - 1):
-        Mb[n_block, sample] = np.cast[np.int32](data[(n_block * 750) + sample]) + offset
+for n_block in range(0, complete_transfers ):
+    for sample in range(0, max_samples_per_transfer ):
+        Mb[n_block, sample] = np.cast[np.int32](data[(n_block * max_samples_per_transfer) + sample]) + offset
     # end
 # end
 
 number_of_bytes_final_transfer = int(number_of_bytes_final_transfer)
 for sample in range(0, int((number_of_bytes_final_transfer / 2) - 1)):
-    Mb[n_block, sample] = np.cast[np.int32](data[((n_block + 1) * 750) + sample]) + offset
+    Mb[n_block, sample] = np.cast[np.int32](data[((n_block + 1) * max_samples_per_transfer) + sample]) + offset
 # end
 
 Ftx = fs / max_samples_per_transfer
 Ttx = 1 / Ftx
 
-
+print(Ftx)
+print(Ttx)
 Mb = m(Mb)
 Mb = np.cast[np.uint16](Mb)
 
 
-# PRUEBA, un solo bloque transmitido
-sock.sendto(Mb[0], (UDP_IP, UDP_PORT))  # Message sending to the established IP and Port
-
 # YA TODA LA CANCION (FALTA PROBAR CON UN SOLO BLOQUE)
 # cada Ttx, se debe enviar un bloque
 # cada envio, debe ser un bloque, es decir MB(indice_A_mover,Enviartodoeste subarray)
-# for block in range(0,total_transfers):
-#    sock.sendto(Mb[block], (UDP_IP,UDP_PORT)) #Message sending to the established IP and Port
-#    time.sleep(Ttx)
+block :int
+
+for block in range(0,total_transfers -1):
+    sock.sendto(Mb[block], (UDP_IP,UDP_PORT)) #Message sending to the established IP and Port
+    time.sleep(Ttx)
 # end
 
 
