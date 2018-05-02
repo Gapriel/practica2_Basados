64e02b7617a49753329187e73dd0b2b7dfcc7cba
Gapriel <ie699356@iteso.mx>
2018-05-01 23:10:39 -0500

    udp status menu implemented
    it works

---
+++ contrib/apps/UDP/UDP.c
@@ -0,0 +1,176 @@
+/*
+ * Copyright (c) 2001-2003 Swedish Institute of Computer Science.
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED 
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * OF SUCH DAMAGE.
+ *
+ * This file is part of the lwIP TCP/IP stack.
+ * 
+ * Author: Adam Dunkels <adam@sics.se>
+ *
+ */
+#include <TCP/TCP.h>
+#include <UDP/UDP.h>
+#include "lwip/opt.h"
+
+#if LWIP_NETCONN
+
+#include "lwip/api.h"
+#include "lwip/sys.h"
+#include "Audio.h"
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "event_groups.h"
+
+uint16_t buffer1[audio_buffer_sizes] = { 2045 };
+uint16_t buffer2[audio_buffer_sizes] = { 2045 };
+extern uint8_t i;
+extern QueueHandle_t port_selection;
+extern EventGroupHandle_t WirlessSpeakers_events;
+extern QueueHandle_t UDP_status_values;
+
+extern struct netconn * newconn;
+
+bool buffer_flag = pdFALSE;
+uint16_t packs_count = 0;
+uint8_t packs_in_theory_to_be_received = 29;
+
+UDP_val_t * UDP_values_to_transport;
+uint8_t pit1flag = 0;
+void PIT1_IRQHandler(void){
+	/* Clear interrupt flag.*/
+	PIT_ClearStatusFlags(PIT, kPIT_Chnl_1, kPIT_TimerFlag);
+	UDP_values_to_transport->received = packs_count;
+	UDP_values_to_transport->losses = (uint16_t)(packs_in_theory_to_be_received - packs_count);
+	UDP_values_to_transport->relation = (uint16_t)((100)*(UDP_values_to_transport->received / packs_in_theory_to_be_received));
+	//tcp_printUDPstatus(tcp_printUDPstatus);
+	BaseType_t pxHigherPriorityTaskWoken = pdFALSE;
+	xQueueSendFromISR(UDP_status_values,&UDP_values_to_transport,&pxHigherPriorityTaskWoken);
+	portYIELD_FROM_ISR(pxHigherPriorityTaskWoken);
+	packs_count = 0;
+	pit1flag = 1;
+}
+
+/*-----------------------------------------------------------------------------------*/
+static void UDP_thread(void *arg) {
+    LWIP_UNUSED_ARG(arg);
+
+    struct netconn *conn;
+      struct netbuf *buf;
+      err_t err;
+      uint16_t* received_port = 0 ;
+      uint16_t port = 50007;
+
+      conn = netconn_new(NETCONN_UDP);
+    while (1)
+    {
+        if ( (*received_port) != 0){
+            port = *received_port;
+            vPortFree(received_port);
+        }
+
+#if LWIP_IPV6
+            conn = netconn_new(NETCONN_UDP_IPV6);
+            netconn_bind(conn, IP6_ADDR_ANY, port);
+#else /* LWIP_IPV6 */
+            netconn_bind(conn, IP_ADDR_ANY, port);
+#endif /* LWIP_IPV6 */
+            LWIP_ERROR("udpecho: invalid conn", (conn != NULL), return;);
+
+#if LWIP_IGMP /* Only for testing of multicast join*/
+            {
+#include "lwip\netif.h"
+
+                ip4_addr_t multiaddr;
+                IP4_ADDR(&multiaddr, 224, 5, 6, 7);
+
+                err = netconn_join_leave_group(conn, &multiaddr, &netif_default->ip_addr, NETCONN_JOIN);
+                LWIP_ERROR("udpecho: join group is failed", (err == ERR_OK), return;);
+            }
+#endif
+            AudioConfig(45);
+            UDP_values_to_transport = pvPortMalloc(sizeof(UDP_val_t));
+            do
+            {
+
+                err = netconn_recv(conn, &buf);
+                if (err == ERR_OK)
+                {
+                	packs_count++;
+                    /*  no need netconn_connect here, since the netbuf contains the address */
+                    if (pdFALSE == buffer_flag)
+                    {
+                        if (netbuf_copy(buf, buffer1,
+                                        sizeof(buffer1))
+                                != buf->p->tot_len)
+                        {
+                            LWIP_DEBUGF(LWIP_DBG_ON, ("netbuf_copy failed\n"));
+                            i = 0;
+                        } else
+                        {
+                            err = netconn_send(conn, buf);
+                            if (err != ERR_OK)
+                            {
+                                LWIP_DEBUGF(
+                                        LWIP_DBG_ON,
+                                        ("netconn_send failed: %d\n", (int)err));
+                            }
+                        }
+
+                        buffer_flag = pdTRUE;
+                        i = 0;
+                    } else
+                    {
+                        if (netbuf_copy(buf, buffer2, sizeof(buffer2)) != buf->p->tot_len)
+                        {
+                            LWIP_DEBUGF(LWIP_DBG_ON, ("netbuf_copy failed\n"));
+                            i = 0;
+                        } else
+                        {
+                            err = netconn_send(conn, buf);
+                            if (err != ERR_OK)
+                            {
+                                LWIP_DEBUGF(
+                                        LWIP_DBG_ON,
+                                        ("netconn_send failed: %d\n", (int)err));
+                            }
+                        }
+
+                        buffer_flag = pdFALSE;
+                        i = 0;
+                    }
+                    netbuf_delete(buf);
+                }
+            }while(pdPASS != xQueueReceive(port_selection,&received_port,0));
+            netconn_close(conn);
+    }
+}
+
+/*-----------------------------------------------------------------------------------*/
+void UDP_init(void) {
+    sys_thread_new("UDP thread", UDP_thread, NULL,
+    DEFAULT_THREAD_STACKSIZE,
+                   DEFAULT_THREAD_PRIO);
+}
+
+#endif /* LWIP_NETCONN */
---
+++ pit.c
@@ -0,0 +1,90 @@
+/*
+ * The Clear BSD License
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided
+ * that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "pit.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+#define PIT_IRQ_ID PIT0_IRQn
+#define PIT_IRQ_ID2 PIT1_IRQn
+/* Get source clock for PIT driver */
+#define LED_INIT() LED_RED_INIT(LOGIC_LED_ON)
+#define LED_TOGGLE() LED_RED_TOGGLE()
+
+
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+
+
+/*!
+ * @brief Main function
+ */
+void PIT_ConfigAndStart(uint8_t channel,uint32_t usecs)
+{
+    /* Structure of initialize PIT */
+    pit_config_t pitConfig;
+
+    /*
+     * pitConfig.enableRunInDebug = false;
+     */
+    PIT_GetDefaultConfig(&pitConfig);
+
+    /* Init pit module */
+    PIT_Init(PIT, &pitConfig);
+
+    /* Set timer period for channel 0 */
+    uint32_t PIT_SOURCE_CLOCK = CLOCK_GetFreq(kCLOCK_BusClk);
+    if(kPIT_Chnl_0 == channel){
+    	PIT_SetTimerPeriod(PIT, kPIT_Chnl_0, USEC_TO_COUNT(usecs, CLOCK_GetFreq(kCLOCK_BusClk)));
+    	/* Enable timer interrupts for channel 0 */
+    	PIT_EnableInterrupts(PIT, kPIT_Chnl_0, kPIT_TimerInterruptEnable);
+    	/* Enable at the NVIC */
+    	EnableIRQ(PIT_IRQ_ID);
+    	/* Start channel 0 */
+        PIT_StartTimer(PIT, kPIT_Chnl_0);
+    }else if(kPIT_Chnl_1 == channel){
+    	PIT_SetTimerPeriod(PIT, kPIT_Chnl_1, MSEC_TO_COUNT(usecs, CLOCK_GetFreq(kCLOCK_BusClk)));
+    	/* Enable timer interrupts for channel 0 */
+    	PIT_EnableInterrupts(PIT, kPIT_Chnl_1, kPIT_TimerInterruptEnable);
+    	/* Enable at the NVIC */
+    	EnableIRQ(PIT_IRQ_ID2);
+    	NVIC_SetPriority(PIT_IRQ_ID2,3);
+    	/* Start channel 0 */
+    	PIT_StartTimer(PIT, kPIT_Chnl_1);
+    }
+}
